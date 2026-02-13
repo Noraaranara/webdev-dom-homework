d10f5538170c77f8d28ef540e8c93c053318e606
noraaranara <anastasiya.solntseva@gmail.com>
2026-02-13 11:02:32 +0300

    finished hw

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import config from 'eslint-config-prettier';
-import plugin from 'eslint-plugin-prettier/recommended';
+import js from '@eslint/js'
+import globals from 'globals'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,    
-      config,    
-      plugin,
-]);
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            globals: globals.browser,
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            eqeqeq: ['error', 'always'],
+        },
+    },
+    {
+        ignores: ['node_modules/', 'dist/', '*.config.js', '.vscode/', '.git/'],
+    },
+    eslintPluginPrettierRecommended,
+    {
+        rules: {
+            'prettier/prettier': [
+                'error',
+                {
+                    tabWidth: 4,
+                    semi: false,
+                    singleQuote: true,
+                },
+            ],
+        },
+    },
+]
