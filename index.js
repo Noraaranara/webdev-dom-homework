aac64bd2b7462dcee69bf7566eeddb9bb61e6496
noraaranara <anastasiya.solntseva@gmail.com>
2026-02-25 13:34:48 +0300

    edited

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-import { fetchAndRenderComments } from './modules/fetchAndRenderComments.js'
 import { loadComments } from './modules/loaders.js'
 import { initClickBtn } from './modules/initListeners.js'
 
 loadComments()
-
-fetchAndRenderComments()
 
 initClickBtn()
